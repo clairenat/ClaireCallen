20fe36e4d075038db41c7df001cf4c80d32c48dc
clairenat <claireca@umich.edu>
2023-12-04 16:44:35 -0500

    got image transition to work a bit with javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+function transition(image) {
+  document.getElementById('scene-1').style.display = "none";
+  document.getElementById('scene-2').style.display = "block";
+}
+
+function revert(image) {
+  document.getElementById('scene-2').style.display = "none";
+  document.getElementById('scene-1').style.display = "block";
+}
